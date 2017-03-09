1ed627fc6aef580d47769672fcf2645918e92bd6
Luke Margolis <lukem1995@yahoo.com>
2017-03-09 17:33:32 -0500

    fixed expression not allowed error

--- app/Models/TweetModel.php
+++ app/Models/TweetModel.php
@@ -16,14 +16,7 @@ class TweetModel
 
 
 
-    private $keys = array(
-    'oauth_access_token' => "385195414-z6qTLwG604bkPJW4b9LuVu4IzbCZFIWwXeD3attL",
-    'oauth_access_token_secret' => "RLAzRpkT2y5AYldnNpG7ChG3SM8YlGa6jDRspRAYbCaoM",
-    'consumer_key' => "freTdl2M7d0UjtiJGtZWbx5Dz",
-    'consumer_secret' => "okBl6vSLed3Gk1LqF7L9Tch587WvE2VlxsAbyRPnJpGP35ul79T"
-    );
 
-    private $twitter = new exchange($keys);
     //$twitter = new exchange($keys);
 
     private $url = 'https://api.twitter.com/1.1/blocks/create.json';
@@ -36,6 +29,14 @@ class TweetModel
 
     public function request()
     {
+
+        $keys = array(
+        "oauth_access_token" => "385195414-z6qTLwG604bkPJW4b9LuVu4IzbCZFIWwXeD3attL",
+        "oauth_access_token_secret" => "RLAzRpkT2y5AYldnNpG7ChG3SM8YlGa6jDRspRAYbCaoM",
+        "consumer_key" => "freTdl2M7d0UjtiJGtZWbx5Dz",
+        "consumer_secret" => "okBl6vSLed3Gk1LqF7L9Tch587WvE2VlxsAbyRPnJpGP35ul79T"
+    );
+        $twitter = new exchange($keys);
         echo $twitter->buildOauth($url, $requestMethod)
             ->setPostfields($postfields)
             ->performRequest();
